c6c50017fa1d00cfe2972809a56470f985499806
linjin <583689381@qq.com>
2019-06-11 05:37:25 +0800

    新增equal方法

--- src/main/java/hello/TestGit.java
+++ src/main/java/hello/TestGit.java
@@ -5,4 +5,9 @@ public class TestGit {
   public static void main(String[] args) {
     System.out.println("test git");
   }
+
+  @Override
+  public boolean equals(Object obj) {
+    return super.equals(obj);
+  }
 }
